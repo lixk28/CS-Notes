3515af1c453adf1899425c02dbeb1d575d35adfd
Niklaus <niklausama@qq.com>
2021-02-10 22:28:16 +0800

    upload my first python program

---
+++ Python/Hello.py
@@ -0,0 +1,12 @@
+'''
+ Description  : 
+ Version      : 
+ Author       : Niklaus
+ Date         : 2021-02-10 22:22:18
+ LastEditor   : Niklaus
+ LastEditTime : 2021-02-10 22:22:47
+ 
+ Copyright (C) 2021 Niklaus. All rights reserved.
+'''
+
+print("Hello, Python!")
