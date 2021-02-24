ef2ae249d392b1e2d198820a41c18e41b0d22dd1
Niklaus <niklausama@qq.com>
2021-02-24 14:35:40 +0800

    update Hello Rust

--- Rust/Hello/target/debug/Hello.d
+++
@@ -1,1 +0,0 @@
-/home/lixk28/Note/Rust/Hello/target/debug/Hello: /home/lixk28/Note/Rust/Hello/src/main.rs
--- Rust/Hello/target/debug/deps/Hello-24b65cce92894d3a.d
+++
@@ -1,5 +0,0 @@
-/home/lixk28/Note/Rust/Hello/target/debug/deps/Hello-24b65cce92894d3a: src/main.rs
-
-/home/lixk28/Note/Rust/Hello/target/debug/deps/Hello-24b65cce92894d3a.d: src/main.rs
-
-src/main.rs:
